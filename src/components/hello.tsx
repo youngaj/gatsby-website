06288cdcaf2b4004978e89c5fabb55bc400ad8cf
Andre Young <dreyoung79@yahoo.com>
2020-05-31 21:27:10 -0400

    Abstract data and add hello text

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -4,27 +4,53 @@ import StyledButton from './styledButton'
 import { useStaticQuery, graphql } from 'gatsby'
 import { makeStyles, useTheme, Theme } from '@material-ui/core/styles'
 import sharedStyles from '../styles/global'
+import info from '../data/info'
+import moment from 'moment'
+import useWindowSize from '../utils/useWindowSize'
 
 const useStyles = makeStyles((theme: Theme) => ({
    ...sharedStyles(theme),
-   uppercase: {
+   heading: {
       textTransform: 'uppercase',
+      marginBottom: '20px',
    },
    container: {
       display: 'grid',
       msGridColumns: '1fr',
       gridTemplateColumns: '1fr',
+      textAlign: 'center',
       [theme.breakpoints.up('md')]: {
+         textAlign: 'left',
          msGridColumns: '1fr 1fr',
          gridTemplateColumns: '1fr 1fr',
       },
+      marginLeft: '54px',
+      marginRight: '54px',
+      paddingLeft: '15px',
+      paddingRight: '15px',
+      '& p': {
+         marginBottom: '10px',
+      },
+   },
+   divider: {
+      display: 'block',
+      marginTop: '35px',
+      marginBottom: '35px',
+      backgroundColor: theme.palette.secondary.main,
+      height: '2px',
+      width: '150px',
+   },
+   textGray: {
+      color: 'rgb(161, 161, 161)',
    },
 }))
 
 const Hello = () => {
    const theme = useTheme()
    const css = useStyles(theme)
-
+   const dimensions = useWindowSize()
+   const age = moment().diff(info.me.dob, 'years')
+   const yearsOfExperience = moment().diff('1999-06-1', 'years')
    const data = useStaticQuery(graphql`
       query {
          profile: file(relativePath: { eq: "profile-1.jpg" }) {
@@ -40,17 +66,44 @@ const Hello = () => {
    return (
       <div className={css.container}>
          <div>
-            <h2>
-               <span className={css.uppercase}>Hello, My name is Andre.</span>
-            </h2>
+            <h2 className={css.heading}>Hello, My name is Andre.</h2>
+            <p>
+               Andre Young, {age} years young, born and raised in the Washington
+               D.C. metro area. Highly motivated senior software engineering
+               manager and tech lead with over {yearsOfExperience} years of
+               development experience.
+            </p>
+            <span className={css.divider}></span>
+            <p className={css.textGray}>
+               As of April 2020 I started a new job with Capital One. Very
+               excited to be joining an awesome team doing awesome work!
+            </p>
+            <p className={css.textGray}>
+               Formerly I worked at NASA's Goddard Space Flight Center, where I
+               spent most of my time crafting and working on cool (and sometimes
+               important but not so cool) projects.
+            </p>
+            <p className={css.textGray}>
+               I also ran a bi-weekly developer meetup at NASA, where we
+               discussed various development topics ranging from code
+               architecture to knowing how to say "No" to a customer and having
+               them say "thank you" afterwards.
+            </p>
+            <p className={css.textGray}>
+               I love development, love learning and sharing the things I've
+               learned with anyone willing to listen. See my resume and other
+               interests below.
+            </p>{' '}
             <StyledButton>Download Resume</StyledButton>
          </div>
-         <div>
-            <Img
-               fluid={data.profile.childImageSharp.fluid}
-               alt={'Profile picture'}
-            />
-         </div>
+         {dimensions.windowWidth > theme.breakpoints.values.md && (
+            <div>
+               <Img
+                  fluid={data.profile.childImageSharp.fluid}
+                  alt={'Profile picture'}
+               />
+            </div>
+         )}
       </div>
    )
 }
--- src/data/info.ts
+++ src/data/info.ts
@@ -1,4 +1,8 @@
 const info = {
+   me: {
+      dob: '1979-09',
+      workExperience: [],
+   },
    nasa: {
       lastDay: '2020/04/20',
    },
