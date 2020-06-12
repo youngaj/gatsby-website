b846342960178e8762a550a1c10ec7d169828eea
Andre Young <dreyoung79@yahoo.com>
2020-06-11 22:16:05 -0400

    Refactor to extract dev group sessions

--- src/components/preview/devGroup.tsx
+++ src/components/preview/devGroup.tsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles, useTheme, Theme } from '@material-ui/core/styles'
 
-import { sharedStyles, colors } from '../../styles/global'
+import { sharedStyles } from '../../styles/global'
 import SiteSection from '../presntation/siteSection'
 import SubHeading from '../presntation/subHeading'
 import CenterDivider from '../presntation/centerDivider'
 import { getVideos } from '../../utils/devGroupService'
 import StyledButton from '../styledButton'
 import { Link } from 'gatsby'
-import ColorBox from '../presntation/colorBox'
+import DevGroupSession from '../devGroupSession'
 
 const useStyles = makeStyles((theme: Theme) => ({
    ...sharedStyles(theme),
@@ -16,19 +16,6 @@ const useStyles = makeStyles((theme: Theme) => ({
       display: 'grid',
       gridGap: theme.spacing(2),
       gridTemplateColumns: '1fr 1fr 1fr',
-   },
-   episode: {
-      display: 'grid',
-      gridTemplateColumns: '1fr',
-      gridGap: theme.spacing(2),
-      padding: theme.spacing(2),
-   },
-   details: {
-      display: 'grid',
-      gridTemplateColumns: '1fr 3fr',
-      gridGap: theme.spacing(2),
-      border: `1px solid ${colors.muted}`,
-      padding: theme.spacing(2),
    },
 }))
 
@@ -59,37 +46,8 @@ const DevGroupPreview = () => {
             <CenterDivider />
          </span>
          <div className={css.container}>
-            {videos.map((episode, index) => (
-               <div
-                  key={`episode-${episode.id}-${index}`}
-                  className={css.episode}
-               >
-                  <div>
-                     <img
-                        src={episode.thumbnails.medium.url}
-                        alt="{episode.title}"
-                     />
-                  </div>
-                  <div className={css.details}>
-                     <ColorBox>
-                        <div>
-                           <div className={[css.pt65, css.wt700].join(' ')}>
-                              {episode.day}
-                           </div>
-                           <div>{episode.monthYear}</div>
-                        </div>
-                     </ColorBox>
-                     <span style={{ textAlign: 'left' }}>
-                        <a
-                           href={episode.link}
-                           target="_blank"
-                           className={css.mutedText}
-                        >
-                           {episode.title}
-                        </a>
-                     </span>
-                  </div>
-               </div>
+            {videos.map((session, index) => (
+               <DevGroupSession session={session} />
             ))}
          </div>
          <div>
