83609252903cea8c55ab4c118b752a51849737cb
Andre Young <dreyoung79@yahoo.com>
2020-06-21 10:41:53 -0400

    Add blog support

---
+++ src/blogs/rebuilding-website.md
@@ -0,0 +1,20 @@
+---
+slug: '/blog/rebuiding-website'
+date: '2020-06-20'
+title: 'Rebuildig my website in Gatsby'
+---
+
+# Blog test
+
+## I can't remember how to write markdown
+
+1. Thinkg of an idea
+1. Write down the idea
+1. Erase the idea and come up with a new one
+1. Pull the old idea from git history
+1. Forget to post it
+1. Think of another idea
+
+```
+This area for code type things
+```
---
+++ src/templates/blogTemplate.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { makeStyles, Theme, useTheme } from '@material-ui/core'
+import { sharedStyles } from '../styles/global'
+
+const useStyles = makeStyles((theme: Theme) => ({
+   ...sharedStyles(theme),
+   container: {
+      maxWidth: '1000px',
+      marginLeft: theme.spacing(4),
+      marginRight: theme.spacing(4),
+   },
+   blogPost: {},
+   blogPostContent: {},
+}))
+
+export default function Template({ data }) {
+   const theme = useTheme()
+   const css = useStyles(theme)
+
+   const { markdownRemark } = data
+   const { frontmatter, html } = markdownRemark
+   return (
+      <div className={css.container}>
+         <div className={css.blogPost}>
+            <h1>{frontmatter.title}</h1>
+            <h2>{frontmatter.date}</h2>
+            <div
+               className={css.blogPostContent}
+               dangerouslySetInnerHTML={{ __html: html }}
+            />
+         </div>
+      </div>
+   )
+}
+
+export const pageQuery = graphql`
+   query($slug: String!) {
+      markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+         html
+         frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            slug
+            title
+         }
+      }
+   }
+`
