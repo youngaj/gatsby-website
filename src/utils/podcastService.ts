8312166c97fb1094c0c1982f9d227b8b8fde7e0d
Andre Young <dreyoung79@yahoo.com>
2020-07-05 00:02:28 -0400

    Create Podcasts Preview with starred episodes

--- src/utils/podcastService.ts
+++ src/utils/podcastService.ts
@@ -1,10 +1,15 @@
 import axios from 'axios'
 
-export const getPodcasts = async () => {
+export const getPodcastInfo = async () => {
    const response = await axios.get(
       'https://andrepodcasts.azurewebsites.net/api/PodcastQueue'
    )
 
    const data = response.data
    return data
+}
+
+export const getStarredEpisodes = async () => {
+   const data = await getPodcastInfo()
+   return data.starred
 }
