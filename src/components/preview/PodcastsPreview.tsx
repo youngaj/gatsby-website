d18494263980e96c57a2a9a1fec36c68250a0900
Andre Young <dreyoung79@yahoo.com>
2021-03-20 22:41:55 -0400

    Show starred, queued and subscribed tabs

--- src/components/preview/PodcastsPreview.tsx
+++ src/components/preview/PodcastsPreview.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { makeStyles, useTheme, Theme } from '@material-ui/core/styles'
 import { sharedStyles, colors } from '../../styles/global'
 import SubHeading from '../presntation/subHeading'
-import CenterDivider from '../presntation/centerDivider'
 import SiteSection from '../presntation/siteSection'
 import { getPodcastInfo, getShoweNotes } from '../../utils/podcastService'
 import StyledButton from '../styledButton'
@@ -10,6 +9,7 @@ import { info } from '../../data/info'
 import { Link } from 'gatsby'
 import { useGlobalCss } from '../../utils/useGlobalCss'
 import useWindowSize from '../../utils/useWindowSize'
+import Podcast from '../podcast'
 
 const useStyles = makeStyles((theme: Theme) => ({
    ...sharedStyles(theme),
@@ -50,13 +50,45 @@ const useStyles = makeStyles((theme: Theme) => ({
          width: '100%',
       },
    },
+   tabs: {
+      display: 'grid',
+      gridTemplateColumns: '1fr 1fr 1fr',
+      gap: '1rem',
+      margin: '1rem',
+      marginTop: '2rem',
+   },
+   tabHeader: {
+      fontSize: '1.5rem',
+   },
+   divider: {
+      display: 'block',
+      marginTop: '1rem',
+      marginBottom: '15px',
+      backgroundColor: theme.palette.secondary.main,
+      height: '2px',
+      width: '150px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+   },
+   count: {
+      fontSize: '1.2rem',
+      color: colors.muted,
+   },
 }))
+
+enum Tab {
+   Starred = 'Starred',
+   Queue = 'Queue',
+   Subscribed = 'Subscribed',
+}
 
 const PodcastsPreview = () => {
    const theme = useTheme()
    const css = useStyles(theme)
    const globalCss = useGlobalCss()
    const twitter = info.me.social.find((x) => x.name === 'Twitter')
+   const displayCount = 6
+   const [visibleTab, setVisibleTab] = useState<Tab>(Tab.Starred)
    const [podcastData, setData] = useState({
       queue: [],
       podcasts: [],
@@ -87,44 +119,127 @@ const PodcastsPreview = () => {
             know of other podcasts you would recommend please send me your
             suggestions at <a href={twitter.link}>{twitter.username}</a>
          </p>
-         <span>
-            <CenterDivider />
-         </span>
-         <div className={css.container}>
-            {podcastData.starred.map((episode, index) => (
-               <div
-                  key={`episode-${episode.title}-${index}`}
-                  className={css.episode}
-               >
-                  <div>
-                     <img
-                        src={`https://static.pocketcasts.com/discover/images/130/${episode.podcastUuid}.jpg`}
-                        alt="{episode.title}"
-                        className={css.episodeIcon}
-                     />
-                  </div>
-                  <div style={{ marginLeft: theme.spacing(2) }}>
-                     <h3 className={css.episodeTitle}>
-                        <a href={episode.url}>{episode.title}</a>
-                     </h3>
-                     {largeScreen && (
-                        <div
-                           style={{
-                              display: 'block',
-                              maxHeight: '200px',
-                              //border: '1px solid yellow',
-                              //maxWidth: '450px',
-                              overflowY: 'auto',
-                           }}
-                           dangerouslySetInnerHTML={{
-                              __html: episode.showNotes,
-                           }}
-                        />
-                     )}
-                  </div>
-               </div>
-            ))}
+         <div className={css.tabs}>
+            <span
+               className={css.tabHeader}
+               onClick={(e) => setVisibleTab(Tab.Starred)}
+            >
+               Starred{' '}
+               <span className={css.count}>({podcastData.starred.length})</span>
+               <span className={css.divider}></span>
+            </span>{' '}
+            <span
+               className={css.tabHeader}
+               onClick={(e) => setVisibleTab(Tab.Queue)}
+            >
+               In Queue{' '}
+               <span className={css.count}>
+                  ({podcastData.queue?.length || 0})
+               </span>
+               <span className={css.divider}></span>
+            </span>{' '}
+            <span
+               className={css.tabHeader}
+               onClick={(e) => setVisibleTab(Tab.Subscribed)}
+            >
+               Subscribed{' '}
+               <span className={css.count}>
+                  ({podcastData.podcasts?.length || 0})
+               </span>
+               <span className={css.divider}></span>
+            </span>
          </div>
+
+         {visibleTab === Tab.Queue && (
+            <div className={css.container}>
+               {podcastData.queue
+                  .slice(0, displayCount)
+                  .map((episode, index) => (
+                     <div
+                        key={`episode-${episode.title}-${index}`}
+                        className={css.episode}
+                     >
+                        <div>
+                           <img
+                              src={`https://static.pocketcasts.com/discover/images/130/${episode.podcast}.jpg`}
+                              alt="{episode.title}"
+                              className={css.episodeIcon}
+                           />
+                        </div>
+                        <div style={{ marginLeft: theme.spacing(2) }}>
+                           <h3 className={css.episodeTitle}>
+                              <a href={episode.url}>{episode.title}</a>
+                           </h3>
+                           {largeScreen && (
+                              <div
+                                 style={{
+                                    display: 'block',
+                                    maxHeight: '200px',
+                                    //border: '1px solid yellow',
+                                    //maxWidth: '450px',
+                                    overflowY: 'auto',
+                                 }}
+                                 dangerouslySetInnerHTML={{
+                                    __html: episode.showNotes,
+                                 }}
+                              />
+                           )}
+                        </div>
+                     </div>
+                  ))}
+            </div>
+         )}
+         {visibleTab === Tab.Subscribed && (
+            <div className={css.container}>
+               {podcastData.podcasts
+                  .slice(0, displayCount)
+                  .map((show, index) => (
+                     <Podcast
+                        data={show}
+                        key={`episode-${show.title}-${index}`}
+                     />
+                  ))}
+            </div>
+         )}
+         {visibleTab === Tab.Starred && (
+            <div className={css.container}>
+               {podcastData.starred
+                  .slice(0, displayCount)
+                  .map((episode, index) => (
+                     <div
+                        key={`episode-${episode.title}-${index}`}
+                        className={css.episode}
+                     >
+                        <div>
+                           <img
+                              src={`https://static.pocketcasts.com/discover/images/130/${episode.podcastUuid}.jpg`}
+                              alt="{episode.title}"
+                              className={css.episodeIcon}
+                           />
+                        </div>
+                        <div style={{ marginLeft: theme.spacing(2) }}>
+                           <h3 className={css.episodeTitle}>
+                              <a href={episode.url}>{episode.title}</a>
+                           </h3>
+                           {largeScreen && (
+                              <div
+                                 style={{
+                                    display: 'block',
+                                    maxHeight: '200px',
+                                    //border: '1px solid yellow',
+                                    //maxWidth: '450px',
+                                    overflowY: 'auto',
+                                 }}
+                                 dangerouslySetInnerHTML={{
+                                    __html: episode.showNotes,
+                                 }}
+                              />
+                           )}
+                        </div>
+                     </div>
+                  ))}
+            </div>
+         )}
          <div style={{ margin: theme.spacing(3) }}>
             <Link to="/podcasts">
                <StyledButton>See All Podcasts</StyledButton>
