5e348cb06993c65b0e26ba59b13e0ea7583eeca2
Andre Young <dreyoung79@yahoo.com>
2021-04-25 16:14:25 -0400

    Add tabs to podcast page

--- src/pages/podcasts.tsx
+++ src/pages/podcasts.tsx
@@ -6,19 +6,77 @@ import Layout from '../components/layout'
 import Nav from '../components/nav'
 import { getPodcastInfo } from '../utils/podcastService'
 import { makeStyles, Theme, useTheme } from '@material-ui/core'
-import { sharedStyles } from '../styles/global'
+import { colors, sharedStyles } from '../styles/global'
 import Podcast from '../components/podcast'
 import SiteSection from '../components/presntation/siteSection'
 import { info } from '../data/info'
 import SubHeading from '../components/presntation/subHeading'
+import { Tab, TabEnum } from '../types'
+import { useWindowSize } from '../hooks/useWindowSize'
 
 const useStyles = makeStyles((theme: Theme) => ({
    ...sharedStyles(theme),
    container: {
       display: 'grid',
-      gridGap: theme.spacing(2),
+      gap: '.5rem',
       gridTemplateColumns: '1fr 1fr',
-      margin: theme.spacing(4),
+      [theme.breakpoints.down('sm')]: {
+         gridTemplateColumns: '1fr',
+         margin: 'auto',
+      },
+   },
+   episode: {
+      display: 'grid',
+      gridTemplateColumns: '90px 1fr',
+      gap: theme.spacing(2),
+      border: `1px solid ${colors.muted}`,
+      borderRadius: '10px',
+      padding: theme.spacing(2),
+      '& div': {
+         textAlign: 'left',
+      },
+      [theme.breakpoints.down('sm')]: {
+         gridTemplateColumns: '1fr',
+         marginBottom: theme.spacing(2),
+      },
+   },
+   episodeTitle: {
+      marginBottom: '2rem',
+      [theme.breakpoints.down('sm')]: {
+         textAlign: 'center',
+      },
+   },
+   episodeIcon: {
+      width: '100%',
+      minWidth: '80px',
+      [theme.breakpoints.down('sm')]: {
+         width: '100%',
+      },
+   },
+
+   tabs: {
+      display: 'grid',
+      gridTemplateColumns: '1fr 1fr 1fr',
+      gap: '1rem',
+      margin: '1rem',
+      marginTop: '2rem',
+   },
+   tabHeader: {
+      fontSize: '1.5rem',
+   },
+   divider: {
+      display: 'block',
+      marginTop: '1rem',
+      marginBottom: '15px',
+      backgroundColor: theme.palette.secondary.main,
+      height: '2px',
+      width: '150px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+   },
+   count: {
+      fontSize: '1.2rem',
+      color: colors.muted,
    },
 }))
 
@@ -33,10 +91,37 @@ const PodcastPage = (props: PageProps) => {
    const css = useStyles(theme)
    const twitter = info.me.social.find((x) => x.name === 'Twitter')
 
-   const [podcastData, setData] = useState({ queue: [], podcasts: [] })
+   const [podcastData, setData] = useState({
+      queue: [],
+      podcasts: [],
+      starred: [],
+   })
+   const [tabs, setTabs] = useState<Tab[]>([])
+   const [visibleTab, setVisibleTab] = useState<TabEnum>(TabEnum.Starred)
+   const windowSize = useWindowSize()
+   const largeScreen =
+      windowSize.width > theme.breakpoints.values.sm ? true : false
+
    useEffect(() => {
       getPodcastInfo().then((data) => {
          setData({ ...data })
+         setTabs([
+            {
+               title: `Stared`,
+               count: data.starred?.length,
+               value: TabEnum.Starred,
+            },
+            {
+               title: `In Queue`,
+               count: data.queue?.length,
+               value: TabEnum.Queue,
+            },
+            {
+               title: `Subscribed`,
+               count: data.podcasts?.length,
+               value: TabEnum.Subscribed,
+            },
+         ])
       })
    }, [])
 
@@ -62,11 +147,109 @@ const PodcastPage = (props: PageProps) => {
                along and/or send me suggestions{' '}
                <a href={twitter.link}>{twitter.username}</a>
             </p>
-            <div className={css.container}>
-               {podcastData.podcasts.sort(podcastSort).map((show, index) => (
-                  <Podcast data={show} key={`episode-${show.title}-${index}`} />
-               ))}
+            <div className={css.tabs}>
+               {tabs.map((tab) => {
+                  const isActive = tab.value === visibleTab
+                  const headerCss = isActive
+                     ? css.tabHeader
+                     : [css.tabHeader, css.mutedText].join(' ')
+                  return (
+                     <span
+                        className={headerCss}
+                        onClick={(e) => setVisibleTab(tab.value)}
+                     >
+                        {tab.title}
+                        <span className={css.count}> ({tab.count})</span>
+                        {isActive && <span className={css.divider}></span>}
+                     </span>
+                  )
+               })}
             </div>
+
+            {visibleTab === TabEnum.Queue && (
+               <div className={css.container}>
+                  {podcastData.queue.map((episode, index) => (
+                     <div
+                        key={`episode-${episode.title}-${index}`}
+                        className={css.episode}
+                     >
+                        <div>
+                           <img
+                              src={`https://static.pocketcasts.com/discover/images/130/${episode.podcast}.jpg`}
+                              alt="{episode.title}"
+                              className={css.episodeIcon}
+                           />
+                        </div>
+                        <div style={{ marginLeft: theme.spacing(2) }}>
+                           <h3 className={css.episodeTitle}>
+                              <a href={episode.url}>{episode.title}</a>
+                           </h3>
+                           {largeScreen && (
+                              <div
+                                 style={{
+                                    display: 'block',
+                                    maxHeight: '200px',
+                                    //border: '1px solid yellow',
+                                    //maxWidth: '450px',
+                                    overflowY: 'auto',
+                                 }}
+                                 dangerouslySetInnerHTML={{
+                                    __html: episode.showNotes,
+                                 }}
+                              />
+                           )}
+                        </div>
+                     </div>
+                  ))}
+               </div>
+            )}
+            {visibleTab === TabEnum.Subscribed && (
+               <div className={css.container}>
+                  {podcastData.podcasts.map((show, index) => (
+                     <Podcast
+                        data={show}
+                        key={`episode-${show.title}-${index}`}
+                     />
+                  ))}
+               </div>
+            )}
+            {visibleTab === TabEnum.Starred && (
+               <div className={css.container}>
+                  {podcastData.starred.map((episode, index) => (
+                     <div
+                        key={`episode-${episode.title}-${index}`}
+                        className={css.episode}
+                     >
+                        <div>
+                           <img
+                              src={`https://static.pocketcasts.com/discover/images/130/${episode.podcastUuid}.jpg`}
+                              alt="{episode.title}"
+                              className={css.episodeIcon}
+                           />
+                        </div>
+                        <div style={{ marginLeft: theme.spacing(2) }}>
+                           <h3 className={css.episodeTitle}>
+                              <a href={episode.url}>{episode.title}</a>
+                           </h3>
+                           {largeScreen && (
+                              <div
+                                 style={{
+                                    display: 'block',
+                                    maxHeight: '200px',
+                                    //border: '1px solid yellow',
+                                    //maxWidth: '450px',
+                                    overflowY: 'auto',
+                                 }}
+                                 dangerouslySetInnerHTML={{
+                                    __html: episode.showNotes,
+                                 }}
+                              />
+                           )}
+                        </div>
+                     </div>
+                  ))}
+               </div>
+            )}
          </SiteSection>
       </Layout>
    )
